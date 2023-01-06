bca706f351317ca53f0695755b880e53a086ad3d
dongmin <ehdals0116@gmail.com>
2023-01-06 11:41:21 +0900

    스프링 3. 트랜잭션 이해

--- 스프링 DB 1편 - 데이터 접근 핵심 원리/jdbc/README.md
+++ 스프링 DB 1편 - 데이터 접근 핵심 원리/jdbc/README.md
@@ -15,3 +15,12 @@
 #### HikariDataSource - 히카리 CP 커넥션 풀 관리 디폴트 10개
 #### 커넥션 풀 - 풀에서 가져다 재사용하기 때문에 커넥션 속도가 빠르다.
 #### 핵심 문법 : DataSource, HikariDataSource 클래스
+
+## 3. 트랜잭션 이해
+#### package hello.jdbc.repository.MemberRepositoryV2
+#### package hello.jdbc.service.MemberServiceV1, V2
+#### 트랜잭션 - ACID 원자성(Atomicity), 일관성(Consistency), 격리성(Isolation), 지속성(Durability)
+#### 자동 커밋, 수동 커밋 - set autocommit false;
+#### 롤백 - rollback
+#### DB락 - 트랜잭션 시 락이 걸림. 락을 획득해야 변경가능. 조회 시는 select for update 로 락 획득 가능
+#### 핵심 문법 : SQL 트랜잭션, DB 락
---
+++ 스프링 DB 1편 - 데이터 접근 핵심 원리/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,29 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    public final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findbyId(fromId);
+        Member toMember = memberRepository.findbyId(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")){
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+}
